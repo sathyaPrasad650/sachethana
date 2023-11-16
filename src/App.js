97d33ad1dc99ee9507c5a73a777e9163bbbded1e
Sathya Prasad S <sathyaprasad650@gmail.com>
2023-11-17 00:33:10 +0530

    second push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import { Home } from "./Component/Home";
-// import { Team } from "./Component/Team";
+import { Team } from "./Component/Team";
 import { Navbar } from "./Component/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Team />
     </div>
   );
 }
